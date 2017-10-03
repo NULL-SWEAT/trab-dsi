da01849b12ec1d5be602f3ff59707a9126378dac
NULL-SWEAT <felipe.094@gmail.com>
2017-10-03 18:34:30 -0300

    First client commit

---
+++ front/src/app/pet.ts
@@ -0,0 +1,9 @@
+export class Pet {
+    constructor(
+        public id: number,
+        public name: string,
+        public race: string,
+        public age: number,
+        public sex: string
+    ) { }
+}
