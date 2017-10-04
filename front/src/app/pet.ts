94ac25141d7a6d0c7800652d67dfeb133d70117d
NULL-SWEAT <felipe.094@gmail.com>
2017-10-04 06:46:44 -0300

    Revert "First client commit"
    This reverts commit da01849b12ec1d5be602f3ff59707a9126378dac.

--- front/src/app/pet.ts
+++
@@ -1,9 +0,0 @@
-export class Pet {
-    constructor(
-        public id: number,
-        public name: string,
-        public race: string,
-        public age: number,
-        public sex: string
-    ) { }
-}
